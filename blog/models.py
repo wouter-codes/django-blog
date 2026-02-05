c7f166f8a83d95de44a92365678d451517f07e28
wouter-codes <wouter.klinkenberg@outlook.com>
2026-02-05 17:51:59 +0000

    Add featured image field to Post model; update index template for image display

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
-from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -9,6 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
--- codestar/settings.py
+++ codestar/settings.py
@@ -42,14 +42,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
-    'blog',
     'django_summernote',
+    'cloudinary',
+    'blog',
     'about',
 ]
 
